aa102f5e8d42526328671ccbe03e4f3ba5f11505
TheGuiff <ergu@mac.com>
2023-06-23 09:58:54 +0200

    feature:prepare_notes

--- Patient-service/src/main/java/com/mediscreen/patientservice/configuration/SwaggerConfig.java
+++ Patient-service/src/main/java/com/mediscreen/patientservice/configuration/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
         public Docket api() {
             return new Docket(DocumentationType.SWAGGER_2)
                     .select()
-                    .apis(RequestHandlerSelectors.basePackage("com.mediscreen.patientservice.controller"))
+                    .apis(RequestHandlerSelectors.basePackage("com.mediscreen.patientservice.web.controller"))
                     .paths(PathSelectors.any())
                     .build();
         }
